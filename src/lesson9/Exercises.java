64bd6cbf9a6159078344fb3cf037dfcf2ad906b7
Alexander Chernomyrdin <chav1961@mail.ru>
2020-04-30 15:46:38 +0500

    Daily commit

---
+++ src/lesson9/Exercises.java
@@ -0,0 +1,25 @@
+package lesson9;
+
+import javax.swing.JFileChooser;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class Exercises {
+
+	public static void main(String[] args) {
+		// TODO make notepad by swing.
+		// JFileChooser
+		JFileChooser 	fc;
+		JTextArea		ta;
+		JScrollPane		sp;
+		JMenuBar		bar;
+		JMenuItem		item;
+		JMenu			menu;
+		JOptionPane		pane;
+	}
+
+}
