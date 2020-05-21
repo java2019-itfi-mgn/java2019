cc7e2716faae9512e09bd84ef899669a3d44eaab
Alexander Chernomyrdin <chav1961@mail.ru>
2020-05-21 16:34:15 +0500

    DAily commit

---
+++ src/lesson12/BatScript.java
@@ -0,0 +1,16 @@
+package lesson12;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class BatScript {
+
+	public static void main(String[] args) throws ScriptException {
+		// TODO Auto-generated method stub
+        final ScriptEngineManager 	factory = new ScriptEngineManager();	
+        final ScriptEngine 			engine = factory.getEngineByName("bat");
+        
+        System.err.println(engine.eval("echo test\nmkdir testdir\nexit")); // Как минимум - test 
+	}
+}
---
+++ src/lesson12/Calculator.java
@@ -0,0 +1,12 @@
+package lesson12;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 2+3*(8-5)
+		// 2*синус(10)*синус(10) + корень(3)
+		
+	}
+
+}
