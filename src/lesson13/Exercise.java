60411faf9c440a0a590df25fe7ac752f750a7c82
Alexander Chernomyrdin <chav1961@mail.ru>
2020-05-28 16:04:27 +0500

    DAily commit

---
+++ src/lesson13/Exercise.java
@@ -0,0 +1,16 @@
+package lesson13;
+
+public class Exercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1. read string from console
+		// 2. convert it to upper or lower case
+		// 3. print it
+		// COntrol:
+		// 1. toUpperCase()
+		// 2. toLowerCase()
+		// 3. amountOfStringProcessed
+	}
+
+}
