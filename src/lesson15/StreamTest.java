61fa6ba79f136bfcc3568ae8594ae225ed1ade6f
Alexander Chernomyrdin <chav1961@mail.ru>
2020-06-11 16:36:04 +0500

    DAily commit

--- src/lesson15/StreamTest.java
+++ src/lesson15/StreamTest.java
@@ -3,6 +3,7 @@ package lesson15;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class StreamTest {
 
@@ -15,6 +16,10 @@ public class StreamTest {
 			.distinct()		// Intermediate 2
 			.count());		// Destination
 		System.err.println("Sum="+IntStream.builder().add(0).add(1).add(2).build().sum());
+		
+		Stream.generate(()->Math.random()).limit(10).forEach((t)->System.err.println("Random="+t));
+		Stream.iterate(1,(n)->2*n).limit(10).forEach((t)->System.err.println("Iterator="+t));
+		StreamSupport.stream(Arrays.asList("123","abc","123","abc","12345","abcde","12345","abcde").spliterator(),false).forEach((t)->System.err.println("Built="+t)); 
 	}
 
 	static Stream<String> createStream() {
